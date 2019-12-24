bbe0588096b0b28170903d54028ba9412df4e239
Aleksa <aleks.tachev@gmail.com>
2019-12-24 22:27:14 +0200

    Fix webpack issues - cannot get /

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -6,7 +6,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, "public"),
         filename: "index_bundle.js",
-        publicPath: "./"
+        publicPath: "/"
     },
     module: {
         rules: [
